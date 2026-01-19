e0215ab3d696f540ed4f828b8538f9da0980d8ff
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-01-19 17:25:59 +0100

    Fix duplication bug in sample

--- sample/src/main/java/me/timschneeberger/reflectionexplorer/sample/SampleMainActivity.kt
+++ sample/src/main/java/me/timschneeberger/reflectionexplorer/sample/SampleMainActivity.kt
@@ -8,6 +8,7 @@ class SampleMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        ReflectionExplorer.instances.clear()
         ReflectionExplorer.instances.addAll(
             TestInstancesProvider.instances +
             this +
