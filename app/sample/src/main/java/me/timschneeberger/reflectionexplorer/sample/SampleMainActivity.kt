326fd7ec0b4e79b64d943c1361ee3a4f787ffff2
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-01-19 17:08:17 +0100

    Fix library

--- app/sample/src/main/java/me/timschneeberger/reflectionexplorer/sample/SampleMainActivity.kt
+++ app/sample/src/main/java/me/timschneeberger/reflectionexplorer/sample/SampleMainActivity.kt
@@ -2,13 +2,11 @@ package me.timschneeberger.reflectionexplorer.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 import me.timschneeberger.reflectionexplorer.ReflectionExplorer
 
 class SampleMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample_main)
 
         ReflectionExplorer.instances.addAll(
             TestInstancesProvider.instances +
